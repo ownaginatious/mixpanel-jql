669076cfab055eacb4655e292579516d870e8c7f
sophstad <srs2231@columbia.edu>
2017-08-20 20:03:51 -0400

    Add reducer functions

--- mixpanel_jql/query.py
+++ mixpanel_jql/query.py
@@ -26,6 +26,26 @@ class Reducer(object):
         return "mixpanel.reducer.sum(%s)" % accessor
 
     @staticmethod
+    def avg(accessor):
+        return "mixpanel.reducer.avg(%s)" % accessor
+
+    @staticmethod
+    def min(accessor):
+        return "mixpanel.reducer.min(%s)" % accessor
+
+    @staticmethod
+    def max(accessor):
+        return "mixpanel.reducer.max(%s)" % accessor
+
+    @staticmethod
+    def null():
+        return "mixpanel.reducer.null()"
+
+    @staticmethod
+    def any():
+        return "mixpanel.reducer.any()"
+
+    @staticmethod
     def numeric_summary(accessor):
         return "mixpanel.reducer.numeric_summary(%s)" % accessor
 
